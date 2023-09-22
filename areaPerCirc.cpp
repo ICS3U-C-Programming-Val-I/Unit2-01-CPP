b0e4ef7dfc6a3801f17110d98718b7d34f7e0a22
Val Ijaola <valian.ijaola@stu.ocsb.ca>
2023-09-22 17:06:38 +0000

    Added areapercirc.cpp

---
+++ areaPerCirc.cpp
@@ -0,0 +1,15 @@
+// Copyright (c) 2023 Val Ijaola All rights reserved.
+//
+// Created by: Val Ijaola
+// Created on: September. 22, 2023
+// This program calculates an the area
+// perimeter of a circle with radius 5.7cm.
+#include <cmath>
+#include <iostream>
+
+int main() {
+    std::cout << "If a circle has the radius of 5.7cm:\n";
+    std::cout << "\n";
+    std::cout << "Area = " << M_PI * pow(5.7, 2) << " cm^2\n";
+    std::cout << "Perimeter = " << 2 * M_PI * 5.7 << " cm\n";
+}
